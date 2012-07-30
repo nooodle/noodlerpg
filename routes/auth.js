6006a04aa5473d694e12731732a556069ff2af03
Jen Fong-Adwent <jen@ednapiranha.com>
2012-07-30 10:39:44 -0400

    added restricted access control

--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,33 @@
 var auth = require('../lib/authenticate');
 var user = require('../lib/user');
+var invitees = require('../config/invitees');
 
 module.exports = function(app, db, nconf, isLoggedIn) {
   // Login
   app.post('/login', function(req, res) {
     auth.verify(req, nconf, function(error, email) {
       if (email) {
-        user.getStats(email, db, function(err, userStat) {
-          for (var name in userStat) {
-            req.session[name] = userStat[name];
-          }
+        if (invitees[email]) {
+          user.getStats(email, db, function(err, userStat) {
+            for (var name in userStat) {
+              req.session[name] = userStat[name];
+            }
 
-          res.redirect('/dashboard');
-        });
+            res.redirect('/dashboard');
+          });
+        } else {
+          res.redirect('/restricted');
+        }
       } else {
         res.redirect('/');
       }
+    });
+  });
+
+  app.get('/restricted', function(req, res) {
+    res.render('restricted', {
+      pageType: 'restricted',
+      title: 'You aren\'t on the Invite List :('
     });
   });
 
